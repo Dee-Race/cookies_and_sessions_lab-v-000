df84b9abe0755684364e506988d2460d85ed64d2
Dee Race <deerace12@gmail.com>
2018-07-05 17:59:58 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,12 @@
 class ProductsController < ApplicationController
 
   def index
-    @cart = Cart.all
+  
   end
 
   def add
-    @item = Item.find(params[:id])
-    cart = session[:cart] || []
-    cart << @item.id
-    session[:cart] cart
+    cart << params[:product]
+    render :index
   end 
 
 
